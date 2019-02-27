889682f8c657e767043b9abdab46205d2cefce3d
pmccall <pmccall33@gmail.com>
2019-02-27 15:53:13 -0600

    user functionality working

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -9,18 +9,13 @@ class UserController < ApplicationController
 		erb :home_index
 	end
 
-	get '/login' do
-		"Home/Login route at Usercontroller"
-		erb :login
-	end
-
 	get '/register' do
 		erb :register
 	end
 
 	post '/register' do
 		puts "hitting register route"
-		
+
 		user = User.find_by username: params[:username]
 
 		if not user
@@ -34,8 +29,8 @@ class UserController < ApplicationController
 
 			session[:message] = {
 				success: true,
-				status: 'good',
-				message: '#{user.username} successfully created and loggin in.'
+				status: "good",
+				message: "#{user.username} successfully created and loggin in."
 			}
 
 			redirect '/user/home'
@@ -43,13 +38,57 @@ class UserController < ApplicationController
 		else
 			session[:message] = {
 				success: true,
-				status: 'bad',
-				message: 'Sorry, username #{params[:username]} is taken.'
+				status: "bad",
+				message: "Sorry, username #{params[:username]} is taken."
 			}
 
 			redirect '/user/register'
 		end
+	end
 
+		
+	get '/login' do
+		"Home/Login route at Usercontroller"
+		erb :login
+	end
+
+	post '/login' do
+		puts "Login POST route <hit class=""></hit>"
+		puts params
+
+		user = User.find_by username: params[:username]
+
+		pw = params[:password]
+
+		if user and user.authenticate(pw)
+			session[:logged_in] = true
+			session[:username] = user.username
+			session[:message] = {
+				success: true,
+				status: "good",
+				message: "Logged in as #{user.username}"
+			}
+			redirect 'user/home'
+		else
+			session[:message] = {
+				success: false,
+				status: "bad",
+				message: "Shucks, username or password incorrect."
+			}
+			redirect 'user/login'
+		end
+	end
+
+	get '/logout' do
+		username = session[:username]
+
+		session.destroy
+		session[:message] = {
+			success: true,
+			status: "neutral",
+			message: "User #{username} logged out."
+		}
+		redirect '/'
 	end
 end
 
