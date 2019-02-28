61ffce392472fd5fdd2a016a3bcf87688db6a34f
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-02-28 14:56:19 -0600

    fully added in all AdminController routes needed from helper app

--- controllers/AdminController.rb
+++ controllers/AdminController.rb
@@ -1,4 +1,4 @@
-class AdminController < ApplicationController 
+class AdminController < ApplicationController	
 
 	before ['/register'] do 
 		if not (session[:logged_in] and session[:is_admin])
@@ -7,9 +7,50 @@ class AdminController < ApplicationController
 		end
 	end
 
+
+	get '/' do 
+		redirect '/'
+	end
+
+
+	get "/login" do 
+		erb :admin_login
+	end
+
+
 	get "/register" do 
 		erb :admin_register 
 	end
+
+
+	get "/logout" do 
+		session.destroy 
+		session[:message] = "Logged out"
+		redirect '/'
+	end
+
+
+	post '/login' do 
+		
+		admin = User.find_by username: params[:username]
+
+		pw = params[:password]
+
+		# if not (admin and admin.authenticate(pw) and admin.is_admin)
+		if not (admin and admin.password == pw and admin.is_admin)
+			session[:message] = "Failed to log in as administrator"
+			redirect '/admin/login'
+		else 
+			session.destroy 
+			session[:logged_in] = true 
+			session[:username] = params[:username]
+			session[:is_admin] = true 
+			session[:message] = "Logged in as administrator #{admin.username}"
+			
+			redirect '/image/new' 		
+		end
+	end
+
 
 	post '/register' do 
 
