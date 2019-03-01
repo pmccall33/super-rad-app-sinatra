8c04e8ee3dde2f76a381c6666fee6ea3d07d81a3
NATHAN LAMONT SMITH <andinto@gmail.com>
2019-03-01 12:02:10 -0600

    added extra prompts

--- controllers/ImageController.rb
+++ controllers/ImageController.rb
@@ -19,9 +19,12 @@ class ImageController < ApplicationController
 			"how this image makes you feel",
 			"the color you associate with this image",
 			"the layout or composition of the image",
-			"the subject depicted in this image"
+			"the subject depicted in this image",
+			"this image",
+			"what fruit this image would be, if it were a fruit",
+			"what name this image would have, if it had a name",
+			"the first thing that comes to mind when you see this image"
 		]
-
 
 		@prompt = "Oops! Error loading image. Click SKIP and move on."
 
